c0d27ad35a3f9fcc4c38f62e4ab072581c4b5331
rasmiranjanpatnaik <121458176+rasmiranjanpatnaik@users.noreply.github.com>
2024-11-18 20:48:26 +0530

    Initial commit

---
+++ src/app/topics/template-form-validation/template-form-validation.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule, JsonPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template-form-validation',
+  standalone: true,
+  imports: [FormsModule, CommonModule, JsonPipe],
+  templateUrl: './template-form-validation.component.html',
+  styleUrls: ['./template-form-validation.component.css'] // Corrected to styleUrls
+})
+export class TemplateFormValidationComponent {
+
+  isFormSubmited: boolean = false;
+
+  userObj: any = {
+    firstName: '',
+    lastName: '',
+    userName: '',
+    city: '',
+    state: '',
+    zipcode: '',
+    isAggree: false 
+  }
+
+  onSubmit(form: NgForm) {
+    debugger;
+    const isFormValid = form.form.valid;
+    this.isFormSubmited = true;
+  }
+  
+}
